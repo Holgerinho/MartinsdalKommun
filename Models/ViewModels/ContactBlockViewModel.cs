0862998e5300ac5e84ae2d77161dd05feb5f7a6a
Ahmadyagoobi10 <ahmadyaqoobi34@gmail.com>
2026-09-10 16:25:40 +0200

    Changed StartPage.cshtml and ContactBlockView.cshtml

---
+++ Models/ViewModels/ContactBlockViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MartinsdalKommun.Models.Pages;
+using EPiServer.Web;
+using Microsoft.AspNetCore.Html;
+
+namespace MartinsdalKommun.Models.ViewModels
+{
+    public class ContactBlockViewModel
+    {
+     
+        public ContentReference? Image { get; set; }
+
+        public string? Heading { get; set; }
+
+        public string? Subheading { get; set; }
+
+        public string? Text { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
