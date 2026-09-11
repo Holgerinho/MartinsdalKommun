8f0ea9a8b1665ca63f8a8a824b31f3e5a4a73da9
Holge <erik.holgersson95@gmail.com>
2026-09-11 15:02:48 +0200

    WIP: add TeaserBlock

--- Models/Blocks/TeaserBlock.cs
+++ Models/Blocks/TeaserBlock.cs
@@ -1,6 +1,39 @@
-﻿namespace MartinsdalKommun.Models.Blocks
+﻿using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MartinsdalKommun.Models.Blocks
 {
+    [ContentType(DisplayName = "Puffblock", 
+        GUID = "f1c2e3d4-5678-90ab-cdef-1234567890ab", 
+        Description = "Ett puffblock med en bild, rubrik och beskrivning.", 
+        AvailableInEditMode = true)]
     public class TeaserBlock
     {
+        [Display(
+            Name = "Rubrik",
+            Description = "Rubriken på puffblocket.",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string? Heading { get; set; }
+
+        [Display(
+            Name = "Text",
+            Description = "Texten i puffblocket.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual string? Text { get; set; }
+
+        [Display(
+            Name = "Bild",
+            Description = "Bilden i puffblocket.",
+            GroupName = SystemTabNames.Content,
+            Order = 30)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference? Image { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 40)]
+        public virtual PageReference Link { get; set; }
     }
 }
---
+++ Models/ViewModels/TeaserBlockViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MartinsdalKommun.Models.ViewModels
+{
+    public class TeaserBlockViewModel
+    {
+
+        public string? Heading { get; set; }
+
+        public string? Text { get; set; }
+
+        public ContentReference? Image { get; set; }
+
+        public PageReference? Link { get; set; }
+    }
+}
