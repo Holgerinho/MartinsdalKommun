177fdcf36a48bec753ea75dea50b9fe0a7269777
Holge <erik.holgersson95@gmail.com>
2026-09-15 08:01:08 +0200

    add order for sitetabnames

--- Infrastructure/SiteTabNames.cs
+++ Infrastructure/SiteTabNames.cs
@@ -1,11 +1,19 @@
 ﻿using EPiServer.DataAbstraction;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MartinsdalKommun.Infrastructure
 {
     [GroupDefinitions]
     public static class SiteTabNames
     {
+        [Display(
+            Name = "Sidhuvud",
+            Order = 40)]
         public const string Header = "Sidhuvud";
+        [Display(
+            Name = "Sidfot",
+            Order = 50)]
         public const string Footer = "Sidfot";
     }
 }
