0094fb404602711feaf6ac6f7529a3d9a6531e68
Frida Padellaro <frida.padellaro@gmail.com>
2026-09-15 16:02:43 +0200

    Add content type to GenericMedia model

--- Models/Media/GenericMedia.cs
+++ Models/Media/GenericMedia.cs
@@ -1,5 +1,9 @@
 ﻿namespace MartinsdalKommun.Models.Media
 {
+    [ContentType(
+        DisplayName = "Generisk media",
+        GUID = "b0f11cf5-ee6c-429f-822d-9294fd672bcf",
+        Description = "Generiska filer som används på webbplatsen.")]
     public class GenericMedia : MediaData
     {
     }
